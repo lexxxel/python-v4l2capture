9013cc07fb0020f63aae790a7d3a036a52afdb04
M. Dietrich <mdt@emdete.de>
2014-12-21 18:28:38 +0100

    make work

--- cap.py
+++ cap.py
@@ -6,76 +6,91 @@ from select import select
 from v4l2capture import Video_device
 from time import time
 from Tkinter import Frame, Button, Tk, Label, Canvas, BOTH, TOP
+from os.path import exists
+from evdev import InputDevice
+
+# TODO:
+# - determine serial
+# - get v4l properties (sizes & fps)
+# - set v4l properties (contrast, hue, sat, ..)
+# - get event from usb dev
 
 class Cap(Frame):
 	def __init__(self):
+		self.serial = 0
+		self.invert = True
+		self.videodevice = '/dev/video1'
 		self.root = Tk()
+		self.root.bind('<Destroy>', self.stop_video)
 		Frame.__init__(self, self.root)
 		self.pack()
+		self.canvas = Canvas(self, width=640, height=480, )
+		self.canvas.pack()
+		self.take = Button(self, text='take!', fg='red', command=self.single_shot)
+		self.take.pack(side='bottom')
+		self.video = None
+		self.start_video()
+
+	def set_pauseimage(self):
 		self.image = fromfile('image.png')
 		self.photo = PhotoImage(self.image)
-		self.canvas = Canvas(self, width=640, height=480, )
 		self.canvas.create_image(320, 240, image=self.photo)
-		self.canvas.pack() #self.canvas.pack(side=TOP, expand=True, fill=BOTH)
-		self.take = Button(self, text="take", fg="red", command=self.start_view)
-		self.take.pack(side="bottom")
-		#self.QUIT = Button(self, text="QUIT", fg="red", command=root.destroy)
-		self.video = None
 
-	def start_view(self):
-		if self.video:
+	def stop_video(self, *args):
+		if self.video is not None:
 			self.video.stop()
 			self.video.close()
 			self.video = None
-		else:
-			self.video = Video_device("/dev/video0")
-			self.video.set_format(640, 480)
-			self.video.create_buffers(30)
-			self.video.queue_all_buffers()
-			self.video.start()
-			self.root.after(0, self.live_view)
+
+	def start_video(self):
+		if self.video is not None:
+			self.stop_video()
+		self.video = Video_device(self.videodevice)
+		self.video.set_format(640, 480)
+		self.video.create_buffers(30)
+		self.video.queue_all_buffers()
+		self.video.start()
+		#width, height, mode = self.video.get_format() # YCbCr
+		self.root.after(1, self.live_view)
 
 	def live_view(self, delta=3.0):
-		if self.video:
-			#size_x, size_y, mode = self.video.get_format() # YCbCr
+		if self.video is not None:
 			select((self.video,), (), ())
 			data = self.video.read_and_queue()
 			self.image = frombytes('RGB', (640, 480), data)
-			#self.image = invert(self.image)
+			if self.invert:
+				self.image = invert(self.image)
 			self.photo = PhotoImage(self.image)
 			self.canvas.create_image(320, 240, image=self.photo)
 			self.root.after(1, self.live_view)
 
 	def single_shot(self):
-			size_x, size_y = self.video.set_format(2592, 1944)
-			mode = 'RGB'
-			#size_x, size_y, mode = self.video.get_format() # YCbCr
-			self.video.create_buffers(1)
-			self.video.queue_all_buffers()
-			self.video.start()
-			select((self.video, ), (), ())
-			data = self.video.read()
-			image = frombytes(mode, (size_x, size_y), data)
-			#image = invert(image)
-			image.save("scanned.jpg")
-			self.video.stop()
+		def go():
+			self.video = Video_device(self.videodevice)
+			try:
+				width, height = self.video.set_format(2592, 1944)
+				mode = 'RGB'
+				self.video.create_buffers(1)
+				self.video.queue_all_buffers()
+				self.video.start()
+				select((self.video, ), (), ())
+				data = self.video.read()
+				image = frombytes(mode, (width, height), data)
+				if self.invert:
+					image = invert(image)
+				filename = 'scanned.{}.jpg'.format(self.serial)
+				self.serial += 1
+				image.save(filename)
+				print filename, 'saved'
+				self.video.stop()
+			finally:
+				self.video.close()
+				self.video = None
+			self.root.after(10, self.start_video)
+		self.stop_video()
+		self.set_pauseimage()
+		self.root.after(10, go)
 
-	def video_take(self, delta=3.0):
-		self.video = Video_device("/dev/video0")
-		try:
-			size_x, size_y = self.video.set_format(640, 480, fourcc='MJPG')
-			self.video.create_buffers(30)
-			self.video.queue_all_buffers()
-			self.video.start()
-			stop_time = time() + delta
-			with open('video.mjpg', 'wb') as f:
-				while stop_time >= time():
-					select((self.video,), (), ())
-					data = self.video.read_and_queue()
-					f.write(data)
-			self.video.stop()
-		finally:
-			self.video.close()
 
 app = Cap()
 app.mainloop()
