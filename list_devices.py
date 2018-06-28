770cbf5623a71ab7018b9e854e4ec7cb7443166a
Alexander Pitzer <pitzer.a@eppendorf.de>
2018-06-28 14:43:10 +0200

    update device list example

--- list_devices.py
+++ list_devices.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 #
 # python-v4l2captureext
 #
@@ -11,38 +11,38 @@
 # required by law.
 
 import os
-import v4l2capture
+import v4l2captureext
 
 def exc_get(f, *args):
 	try:
 		return f(*args)
-	except Exception, e:
+	except Exception as e:
 		return str(e)
 
 file_names = [x for x in os.listdir("/dev") if x.startswith("video")]
 file_names.sort()
 for file_name in file_names:
 	path = "/dev/" + file_name
-	print path
+	print (path)
 	try:
-		video = v4l2capture.Video_device(path)
+		video = v4l2captureext.Video_device(path)
 		driver, card, bus_info, capabilities = video.get_info()
-		print "\tDriver:", driver
-		print "\tCard:", card
-		print "\tBus-info:", bus_info
-		print "\tCapabilities:", ", ".join(capabilities)
+		print("\tDriver:", driver)
+		print("\tCard:", card)
+		print("\tBus-info:", bus_info)
+		print("\tCapabilities:", ", ".join(map(str, capabilities)))
 		width, height, fourcc = video.get_format()
-		print "\tWidth:", width
-		print "\tHeight:", height
-		print "\tFourcc:", fourcc
+		print("\tWidth:", width)
+		print("\tHeight:", height)
+		print("\tFourcc:", fourcc)
 		for cap in exc_get(video.get_framesizes, fourcc):
-			print "\tFrame-size:", cap
+			print("\tFrame-size:", cap)
 		for cap in exc_get(video.get_frameintervals, fourcc, width, height):
-			print "\tFrame-interval:", cap
-		print "\tAuto-white-balance:", exc_get(video.get_auto_white_balance)
-		print "\tWhite-balance-temperature:", exc_get(video.get_white_balance_temperature)
-		print "\tAuto-exposure:", exc_get(video.get_exposure_auto)
-		print "\tExposure-absolute:", exc_get(video.get_exposure_absolute)
-		print "\tAuto-focus:", exc_get(video.get_focus_auto)
+			print("\tFrame-interval:", cap)
+		print("\tAuto-white-balance:", exc_get(video.get_auto_white_balance))
+		print("\tWhite-balance-temperature:", exc_get(video.get_white_balance_temperature))
+		print("\tAuto-exposure:", exc_get(video.get_exposure_auto))
+		print("\tExposure-absolute:", exc_get(video.get_exposure_absolute))
+		print("\tAuto-focus:", exc_get(video.get_focus_auto))
 	finally:
 		video.close()
